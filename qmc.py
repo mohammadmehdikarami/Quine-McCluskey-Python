86c7348a6f6fe22e6f3d8a4177b277f0ec7a85d7
Mohammadmehdi Karami <mohammadmehdi.k82@gmail.com>
2022-12-14 01:43:31 +0330

    Give boolean function don't cares from user

--- qmc.py
+++ qmc.py
@@ -38,3 +38,33 @@ while True:
         print('Try again!\n')
 num_of_minterms = len(minterms)
 print(num_of_minterms, minterms)
+
+# Give boolean function don't cares
+while True:
+    print('Enter boolean function don\'t cares (or enter -1):')
+    dont_cares = input()
+    if ',' in dont_cares:
+        dont_cares = dont_cares.replace(',', ' ')
+    dont_cares = dont_cares.split()
+    try:
+        dont_cares = [int(d) for d in dont_cares]
+        if len(dont_cares) == 1 and dont_cares[0] == -1:
+            dont_cares = list()
+            break
+        if len(dont_cares) != len(set(dont_cares)):
+            print('Try again!\n')
+            continue
+        if max(dont_cares) >= 2 ** num_of_variables:
+            print('Try again!\n')
+            continue
+        if min(dont_cares) < 0:
+            print('Try again!\n')
+            continue
+        if len(set(minterms) & set(dont_cares)) > 0:
+            print('Try again!\n')
+            continue
+        break
+    except:
+        print('Try again!\n')
+num_of_dont_cares = len(dont_cares)
+print(num_of_dont_cares, dont_cares)
