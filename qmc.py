ea6caf0c340ba318e7e4d4b873da2ef09806ef37
Mohammadmehdi Karami <mohammadmehdi.k82@gmail.com>
2022-12-14 01:20:31 +0330

    Give boolean function minterms from user

--- qmc.py
+++ qmc.py
@@ -14,3 +14,27 @@ while True:
     break
 num_of_variables = len(variables)
 print(num_of_variables, variables)
+
+# Give boolean function minterms
+while True:
+    print('Enter boolean function minterms:')
+    minterms = input()
+    if ',' in minterms:
+        minterms = minterms.replace(',', ' ')
+    minterms = minterms.split()
+    try:
+        minterms = [int(m) for m in minterms]
+        if len(minterms) != len(set(minterms)):
+            print('Try again!\n')
+            continue
+        if max(minterms) >= 2 ** num_of_variables:
+            print('Try again!\n')
+            continue
+        if min(minterms) < 0:
+            print('Try again!\n')
+            continue
+        break
+    except:
+        print('Try again!\n')
+num_of_minterms = len(minterms)
+print(num_of_minterms, minterms)
